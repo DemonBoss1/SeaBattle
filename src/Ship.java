0529a9a00728c034acce21e98e05ce3eff3492a8
DemonBoss1 <121947683+DemonBoss1@users.noreply.github.com>
2023-09-14 09:22:25 +0300

    Add class ship

--- src/Ship.java
+++ src/Ship.java
@@ -3,6 +3,12 @@ import java.util.ArrayList;
 public class Ship {
     Battlefield battlefield;
     ArrayList<Position> ships;
+    int length=0;
+    public Ship(Battlefield battlefield, int i, int j){
+        this.battlefield=battlefield;
+        ships.add(new Position(i, j));
+        length++;
+    }
 
 }
 class Position{
